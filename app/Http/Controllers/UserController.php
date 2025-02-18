08fa53d406495d31795c9b17f4368048bacfc0f5
Dhnillhaq <yakulhaq@gmail.com>
2025-02-18 17:15:48 +0700

    menambahkan halaman user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function profil($id, $name){
+        return view('user.profil')
+        ->with('id' , $id)
+        ->with('name' , $name);
+    }
+}
---
+++ resources/views/user/profil.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Halaman User</title>
+</head>
+<body>
+    <p>Hai {{ $name }} dengan id : {{ $id }}</p>
+</body>
+</html>
