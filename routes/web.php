dcb30ac2ce9ddbb7ec9d30b1b1df956359568889
Dhnillhaq <yakulhaq@gmail.com>
2025-02-25 20:37:48 +0700

    CRUD dengan DB Facade

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BeautyHealthController;
 use App\Http\Controllers\FoodBeverageController;
 use App\Http\Controllers\HomeCareController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -20,7 +21,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+// Route::get('/', [HomeController::class, 'index']);
+Route::get('/', function () {
+    return view('Welcome');
+});
+
+Route::get('/level', [LevelController::class, 'index']);
 
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [FoodBeverageController::class, 'index']);
