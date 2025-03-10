93077da258d0541183a669f59ec20693560ad382
Dhnillhaq <yakulhaq@gmail.com>
2025-03-10 21:53:19 +0700

    jQuery DataTable pada AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,27 +23,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', [HomeController::class, 'index']);
-Route::get('/', function () {
-    return view('Welcome');
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::get('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/level', [WelcomeController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::get('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [FoodBeverageController::class, 'index']);
-    Route::get('/beauty-health', [BeautyHealthController::class, 'index']);
-    Route::get('/home-care', [HomeCareController::class, 'index']);
-    Route::get('/baby-kid', [BabyKidController::class, 'index']);
-});
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profil']);
+// Route::prefix('category')->group(function () {
+//     Route::get('/food-beverage', [FoodBeverageController::class, 'index']);
+//     Route::get('/beauty-health', [BeautyHealthController::class, 'index']);
+//     Route::get('/home-care', [HomeCareController::class, 'index']);
+//     Route::get('/baby-kid', [BabyKidController::class, 'index']);
+// });
 
-Route::get('/transaction', [TransactionController::class, 'index']);
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'profil']);
+
+// Route::get('/transaction', [TransactionController::class, 'index']);
