56603a212454b5e11dcf2462b8aa6ba2ab531a12
Dhnillhaq <yakulhaq@gmail.com>
2025-03-11 10:05:17 +0700

    Layouting & DataTables m_kategori

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -4,7 +4,7 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-                <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a>
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a>
             </div>
         </div>
         <div class="card-body">
@@ -14,12 +14,12 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error')}}</div>
             @endif
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_kategori">
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Level Kode</th>
-                        <th>Level Nama</th>
+                        <th>Kategori Kode</th>
+                        <th>Kategori Nama</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -32,15 +32,15 @@
 @push('js')
     <script>
         $(document).ready(function () {
-            var dataLevel = $('#table_level').DataTable({
+            var dataKategori = $('#table_kategori').DataTable({
                 // serverSide: true, jika ingin menggunakan server side processing
                 serverSide: true,
                 ajax: {
-                    "url": "{{ url('level/list') }}",
+                    "url": "{{ url('kategori/list') }}",
                     "dataType": "json",
                     "type": "POST",
                     "data": function (d) {
-                        d.level_id = $('#level_id').val();
+                        d.kategori_id = $('#kategori_id').val();
                     }
                 },
                 columns: [
@@ -51,14 +51,14 @@
                         orderable: false,
                         searchable: false
                     }, {
-                        data: "level_kode",
+                        data: "kategori_kode",
                         className: "",
                         // orderable: true, jika ingin kolom ini bisa diurutkan
                         orderable: true,
                         // searchable: true, jika ingin kolom ini bisa dicari
                         searchable: true
                     }, {
-                        data: "level_nama",
+                        data: "kategori_nama",
                         className: "",
                         orderable: true,
                         searchable: true
