bf0c0a78fcbee8ad4571a2c1db52c0ed086a177c
Dhnillhaq <yakulhaq@gmail.com>
2025-03-26 23:57:47 +0700

    feat : add method for Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -15,23 +19,23 @@ class AuthController extends Controller
         return view('auth.login');
     }
     public function postlogin(Request $request)
-     {
-         if ($request->ajax() || $request->wantsJson()) {
-             $credentials = $request->only('username', 'password');
-             if (Auth::attempt($credentials)) {
-                 return response()->json([
-                     'status' => true,
-                     'message' => 'Login Berhasil',
-                     'redirect' => url('/')
-                 ]);
-             }
-             return response()->json([
-                 'status' => false,
-                 'message' => 'Login Gagal'
-             ]);
-         }
-         return redirect('login');
-     }
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $credentials = $request->only('username', 'password');
+            if (Auth::attempt($credentials)) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Login Berhasil',
+                    'redirect' => url('/')
+                ]);
+            }
+            return response()->json([
+                'status' => false,
+                'message' => 'Login Gagal'
+            ]);
+        }
+        return redirect('login');
+    }
 
     public function logout(Request $request)
     {
@@ -39,5 +43,47 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('login');
+    }
+
+    public function register()
+    {
+        if (Auth::check()) {
+            return redirect('/');
+        }
+
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('auth.register')->with('level', $level);
+    }
+
+    public function postRegister(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama'     => 'required|string|max:100',
+                'password' => 'required|min:5'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+
+            $user = UserModel::create($request->all());
+            Auth::login($user);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Register Berhasil'
+            ]);
+        }
+        redirect('/');
     }
 }
