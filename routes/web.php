73ca1e753460d045f91ea4ed67941c08d9ee0c6c
Dhnillhaq <yakulhaq@gmail.com>
2025-04-19 14:19:59 +0700

    feat : add new routes to handle change photo profile feature

--- routes/web.php
+++ routes/web.php
@@ -36,16 +36,16 @@ Route::post('register', [AuthController::class, 'postRegister']);
 
 Route::middleware('auth')->group(function () { //
     Route::get('/', [WelcomeController::class, 'index']);
+    
+    Route::get('/profil', [UserController::class, 'profil']);
+    Route::get('/profil/change_photo', [UserController::class, 'change_photo']);
+    Route::post('/profil/change_profil', [UserController::class, 'change_profil'])->name('profile.upload');
 
-    Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
-    Route::delete('/{id}', [BarangController::class, 'destroy']);
 
     Route::middleware(['authorize:ADM'])->group(function () {
         Route::group(['prefix' => 'level'], function () {
             Route::get('/', [LevelController::class, 'index']);
             Route::post('/list', [LevelController::class, 'list']);
-            Route::get('/create', [LevelController::class, 'create']);
-            Route::post('/', [LevelController::class, 'store']);
             Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
             Route::post('/ajax', [LevelController::class, 'store_ajax']);
             Route::get('/{id}', [LevelController::class, 'show']);
@@ -68,14 +68,8 @@ Route::middleware('auth')->group(function () { //
         Route::group(['prefix' => 'barang'], function () {
             Route::get('/', [BarangController::class, 'index']);
             Route::post('/list', [BarangController::class, 'list']);
-            Route::get('/create', [BarangController::class, 'create']);
-            Route::post('/', [BarangController::class, 'store']);
             Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
             Route::post('/ajax', [BarangController::class, 'store_ajax']);
-            Route::get('/{id}', [BarangController::class, 'show']);
-            Route::get('/{id}/edit', [BarangController::class, 'edit']);
-            Route::put('/{id}', [BarangController::class, 'update']);
-            Route::delete('/{id}', [BarangController::class, 'destroy']);
             Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
             Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
@@ -94,10 +88,6 @@ Route::middleware('auth')->group(function () { //
             Route::post('/list', [KategoriController::class, 'list']);
             Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
             Route::post('/ajax', [KategoriController::class, 'store_ajax']);
-            Route::get('/{id}', [KategoriController::class, 'show']);
-            Route::get('/{id}/edit', [KategoriController::class, 'edit']);
-            Route::put('/{id}', [KategoriController::class, 'update']);
-            Route::delete('/{id}', [KategoriController::class, 'destroy']);
             Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
             Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
@@ -134,14 +124,26 @@ Route::middleware('auth')->group(function () { //
         Route::group(['prefix' => 'supplier'], function () {
             Route::get('/', [SupplierController::class, 'index']);
             Route::post('/list', [SupplierController::class, 'list']);
-            Route::get('/create', [SupplierController::class, 'create']);
-            Route::post('/', [SupplierController::class, 'store']);
             Route::get('/create_ajax', [SupplierController::class, 'create_ajax']);
             Route::post('/ajax', [SupplierController::class, 'store_ajax']);
-            Route::get('/{id}', [SupplierController::class, 'show']);
-            Route::get('/{id}/edit', [SupplierController::class, 'edit']);
-            Route::put('/{id}', [SupplierController::class, 'update']);
-            Route::delete('/{id}', [SupplierController::class, 'destroy']);
+            Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']);
+            Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);
+            Route::get('/{id}/show_ajax', [SupplierController::class, 'show_ajax']);
+            Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);
+            Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']);
+            Route::get('/import', [SupplierController::class, 'import']);
+            Route::post('/import_ajax', [SupplierController::class, 'import_ajax']);
+            Route::get('/export_excel', [SupplierController::class, 'export_excel']);
+            Route::get('/export_pdf', [SupplierController::class, 'export_pdf']);
+        });
+    });
+
+    Route::middleware(['authorize:ADM,MNG'])->group(function () {
+        Route::group(['prefix' => 'transaksi'], function () {
+            Route::get('/', [SupplierController::class, 'index']);
+            Route::post('/list', [SupplierController::class, 'list']);
+            Route::get('/create_ajax', [SupplierController::class, 'create_ajax']);
+            Route::post('/ajax', [SupplierController::class, 'store_ajax']);
             Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']);
             Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);
             Route::get('/{id}/show_ajax', [SupplierController::class, 'show_ajax']);
@@ -157,6 +159,4 @@ Route::middleware('auth')->group(function () { //
 
 
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profil']);
-
-// Route::get('/transaction', [TransactionController::class, 'index']);
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'profil']);
