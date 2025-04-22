3a932198a669788ecff757e65ba4de4370b42e17
Dhnillhaq <yakulhaq@gmail.com>
2025-04-22 20:08:21 +0700

    feat : list stok page

---
+++ resources/views/stok/index.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.template')
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <button onclick="modalAction('{{ url('/stok/import') }}')" class="btn btn-info">Import Stok</button>
+                <a href="{{ url('/stok/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export
+                    Stok</a>
+                <a href="{{ url('/stok/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export
+                    Stok</a>
+                <button onclick="modalAction('{{ url('/stok/create_ajax') }}')" class="btn btn-success">Tambah Ajax</button>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success')}}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error')}}</div>
+            @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select name="filter_kategori" class="form-control form-control-sm filter_kategori">
+                                <option value="">- Semua -</option>
+                                @foreach($kategori as $k)
+                                    <option value="{{ $k->kategori_id }}">{{ $k->kategori_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Kategori Barang</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_stok">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Tanggal Stok</th>
+                        <th>Nama Supplier</th>
+                        <th>Nama Barang</th>
+                        <th>Jumlah Stok Barang</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+@endsection
+
+@push('css')
+@endpush
+
+@push('js')
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function () {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataStok;
+        $(document).ready(function () {
+            dataStok = $('#table_stok').DataTable({
+                // serverSide: true, jika ingin menggunakan server side processing
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    "url": "{{ url('stok/list') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                    "data": function (d) {
+                        d.kategori_id = $('.filter_kategori').val();
+                    }
+                },
+                columns: [
+                    {
+                        // nomor urut dari laravel datatable addIndexColumn()
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    }, {
+                        data: "stok_tanggal",
+                        className: "",
+                        // orderable: true, jika ingin kolom ini bisa diurutkan
+                        orderable: true,
+                        // searchable: true, jika ingin kolom ini bisa dicari
+                        searchable: true
+                    }, {
+                        data: "supplier.supplier_nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    }, {
+                        // mengambil data barang hasil dari ORM berelasi
+                        data: "barang.barang_nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    }, {
+                        // mengambil data barang hasil dari ORM berelasi
+                        data: "stok_jumlah",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    }, {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+
+            $('.filter_kategori').change(function () {
+                dataStok.draw(); // yang benar sesuai variabel DataTable kamu
+            });
+
+        });
+    </script>
+@endpush
