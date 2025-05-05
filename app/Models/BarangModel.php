f5b97dc33462abc625be00c74e338f37f15e103b
Dhnillhaq <yakulhaq@gmail.com>
2025-05-05 18:58:21 +0700

    feat : controller and new method for crud barang data

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
@@ -14,23 +15,37 @@ class BarangModel extends Model
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
 
-    protected $fillable = ['barang_kode','barang_nama','harga_beli','harga_jual','kategori_id'];
+    protected $fillable = [
+        'barang_kode',
+        'barang_nama',
+        'harga_beli',
+        'harga_jual',
+        'kategori_id',
+        'image'
+    ];
 
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
     }
 
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
+    }
+
     public function getStok()
     {
         $stokMasuk = DB::table('t_stok')
             ->where('barang_id', $this->barang_id)
-            ->sum('stok_jumlah'); 
+            ->sum('stok_jumlah');
 
         $stokKeluar = DB::table('t_penjualan_detail')
             ->where('barang_id', $this->barang_id)
-            ->sum('jumlah'); 
-        
+            ->sum('jumlah');
+
         $sisaStok = $stokMasuk - $stokKeluar;
         return $sisaStok;
     }
