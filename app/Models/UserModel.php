a94275760d053538fa3309f0bd027215cb9f72eb
Dhnillhaq <yakulhaq@gmail.com>
2025-05-05 10:55:54 +0700

    feat :  image attribute with Eloquent Accessor

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; 
-use Tymon\JWTAuth\Contracts\JWTSubject;// implementasi class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject; // implementasi class Authenticatable
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -25,7 +26,16 @@ class UserModel extends Authenticatable implements JWTSubject
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 'profil_images', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'image',
+        'profil_images',
+        'created_at',
+        'updated_at'
+    ];
 
     protected $hidden = ['password']; // jangan ditampilkan saat select
     protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
@@ -40,6 +50,14 @@ class UserModel extends Authenticatable implements JWTSubject
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
+    }
+
+
     public function getRole()
     {
         return $this->level->level_kode;
@@ -53,5 +71,4 @@ class UserModel extends Authenticatable implements JWTSubject
     {
         return $this->level->level_kode == $role;
     }
-
 }
